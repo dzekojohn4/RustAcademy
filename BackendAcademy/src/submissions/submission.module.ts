12caa80f4f471673ead1c918048046508265aeda
koder-ns <nabilasani999@gmail.com>
2026-06-27 09:42:44 +0100

    grading system

--- BackendAcademy/src/submissions/submission.module.ts
+++ BackendAcademy/src/submissions/submission.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SubmissionController } from './submission.controller';
 import { SubmissionService } from './submission.service';
+import { GradingResultService } from './grading-result.service';
+import { GradingResultRepository } from './grading-result.repository';
 
 @Module({
   controllers: [SubmissionController],
-  providers: [SubmissionService],
-  exports: [SubmissionService],
+  providers: [SubmissionService, GradingResultService, GradingResultRepository],
+  exports: [SubmissionService, GradingResultService],
 })
 export class SubmissionModule {}
